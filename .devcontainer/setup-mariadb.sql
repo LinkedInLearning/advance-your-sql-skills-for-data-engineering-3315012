d480f1c8e14409bbcbb69fdc6acb82ca28d0e8ae
Vinoo Ganesh <vinoo.ganesh@gmail.com>
2023-07-12 10:52:10 -0400

    Updating devcontainer

--- .devcontainer/setup-mariadb.sql
+++ .devcontainer/setup-mariadb.sql
@@ -1,8 +1,9 @@
 -- Using the root user, grant access to the mariadb user.
-GRANT ALL PRIVILEGES ON *.* TO 'mariadb'@'%' IDENTIFIED BY 'mariadb';
+GRANT ALL PRIVILEGES ON *.* TO 'mariadb' @'%' IDENTIFIED BY 'mariadb';
 
 -- Create a table in the database
 USE mariadb;
+
 CREATE TABLE MyTable (
   ID INTEGER NOT NULL AUTO_INCREMENT,
   Item VARCHAR(255),
@@ -10,4 +11,9 @@ CREATE TABLE MyTable (
 );
 
 -- Populate the table
-INSERT INTO MyTable (Item) VALUES ('One'), ('Two'), ('Three');
+INSERT INTO
+  MyTable (Item)
+VALUES
+  ('One'),
+  ('Two'),
+  ('Three');
