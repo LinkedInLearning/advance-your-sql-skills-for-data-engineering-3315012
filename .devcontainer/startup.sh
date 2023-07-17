a6c8b81625ed4082d0e5abc90c5a9b145669708f
Vinoo Ganesh <vinoo.ganesh@gmail.com>
2023-07-17 16:59:53 -0400

    devcontainer updates

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,5 +1,10 @@
 # Run an initial setup script for the MariaDB database
 if [ -f .devcontainer/setup-mariadb.sql ]; then
-  sleep 10
+  sleep 5
   mysql -h 127.0.0.1 -uroot -pmariadb < .devcontainer/setup-mariadb.sql
 fi
+
+# mkdir -p /home/vscode/.dbt
+# touch /home/vscode/.dbt/profiles.yml
+
+# echo """
