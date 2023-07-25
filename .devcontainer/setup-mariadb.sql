29d403bfdc36cf8bfd2baffd82a273fb2cb9defc
a <a@as-MacBook-Pro.local>
2023-07-25 13:58:23 -0400

    Updating

--- .devcontainer/setup-mariadb.sql
+++ .devcontainer/setup-mariadb.sql
@@ -1,19 +1,37 @@
 -- Using the root user, grant access to the mariadb user.
 GRANT ALL PRIVILEGES ON *.* TO 'mariadb' @'%' IDENTIFIED BY 'mariadb';
 
--- Create a table in the database
-USE mariadb;
+-- Intro DBT Setup
+CREATE OR REPLACE DATABASE chapter1 COMMENT 'intro';
+USE intro;
 
-CREATE TABLE MyTable (
-  ID INTEGER NOT NULL AUTO_INCREMENT,
-  Item VARCHAR(255),
-  PRIMARY KEY (ID)
-);
+-- Chapter 2: 911
+CREATE OR REPLACE DATABASE chapter2 COMMENT 'Chapter 2';
+USE chapter2;
+
+-- Chapter 3: Crypto
+CREATE OR REPLACE DATABASE chapter3 COMMENT 'Chapter 3';
+USE chapter3;
+
+-- Chapter 4: Construction
+CREATE OR REPLACE DATABASE chapter4 COMMENT 'Chapter 4';
+USE chapter4;
+
+-- Chapter 5: Testing
+CREATE OR REPLACE DATABASE chapter5 COMMENT 'Chapter 5';
+USE chapter5;
+
+
+-- CREATE TABLE MyTable (
+--   ID INTEGER NOT NULL AUTO_INCREMENT,
+--   Item VARCHAR(255),
+--   PRIMARY KEY (ID)
+-- );
 
 -- Populate the table
-INSERT INTO
-  MyTable (Item)
-VALUES
-  ('One'),
-  ('Two'),
-  ('Three');
+-- INSERT INTO
+--   MyTable (Item)
+-- VALUES
+--   ('One'),
+--   ('Two'),
+--   ('Three');
