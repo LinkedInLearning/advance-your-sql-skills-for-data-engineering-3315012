5dc180974792248c2db39fc91e1014283c3daea8
Vinoo Ganesh <vinooganesh@users.noreply.github.com>
2023-07-28 18:29:24 +0000

    Change startup

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,7 +1,2 @@
-# Run an initial setup script for the MariaDB database
-if [ -f .devcontainer/setup-mariadb.sql ]; then
-  sleep 10
-  mysql -h 127.0.0.1 -uroot -pmariadb < .devcontainer/setup-mariadb.sql
-fi
-
+mysql -h 127.0.0.1 -uroot -pmariadb < .devcontainer/setup-mariadb.sql
 echo 'export DBT_PROFILES_DIR="/workspace"'  >> ~/.bash_profile
